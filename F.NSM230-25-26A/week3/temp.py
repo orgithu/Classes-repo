9ceb84fdd113699cdb8fdb7fb5251eaf503b7618
orgilgdg <145784538+orgithu@users.noreply.github.com>
2026-03-30 13:48:50 +0800

    Delete F.NSM230-25-26A directory

--- F.NSM230-25-26A/week3/temp.py
+++
@@ -1,2 +0,0 @@
-from math import pow
-print(pow(29,(25%11)),11)
--- F.NSM230-25-26A/week5/temp.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-def generate_key(length):
-    # Generate truly random bytes
-    return os.urandom(length)
-print("key: ", generate_key(1))
